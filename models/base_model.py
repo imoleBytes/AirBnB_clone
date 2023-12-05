7704bf8913cc0aa992b62b814912a1c76d422384
imole <dremkay71@gmail.com>
2023-12-05 17:30:04 +0100

    task4: Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class BaseModel:
 	"""BaseModel defines all common attributes/methods for other classes"""
-	def __init__(self) -> None:
+	def __init__(self, *args, **kwargs):
 		"""initializes attributes
 		Args:
 			id - an uuid when an instance is created.
@@ -13,9 +13,19 @@ class BaseModel:
 			updated_at: the current datetime when an instance is created
 			and it will be updated every time you change your object.
 		"""
-		self.id = str(uuid4())
-		self.created_at = datetime.today()
-		self.updated_at = datetime.today()
+		if len(kwargs) == 0:
+			self.id = str(uuid4())
+			self.created_at = datetime.today()
+			self.updated_at = datetime.today()
+		else:
+			for key, value in kwargs.items():
+				if key != "__class__":
+					if key in ["created_at", "updated_at"]:
+						key_obj = datetime.strptime(kwargs[key], "%Y-%m-%dT%H:%M:%S.%f")
+						setattr(self, key, key_obj)
+					else:
+						setattr(self, key, value)
+
 
 	def __str__(self) -> str:
 		"""returns the string representio of the object"""
@@ -27,6 +37,7 @@ class BaseModel:
 
 	def to_dict(self):
 		"""returns a dictionary containing all keys/values of the instance"""
-		self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+		self.created_at = str(self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f"))
+		self.updated_at = str(self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f"))
 		self.__dict__["__class__"] = self.__class__.__name__
 		return self.__dict__
