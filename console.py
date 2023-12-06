e4f45d775d128fa7c2cd48b1ef655686cbe745ca
imole <dremkay71@gmail.com>
2023-12-06 17:35:44 +0100

    task7: not completed

--- console.py
+++ console.py
@@ -1,23 +1,47 @@
 #!/usr/bin/python3
 
-import cmd
+import cmd, sys
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
-    """Prompt the user with "(hbnb)" before each command."""
-    prompt = "(hbnb)"
+	"""Prompt the user with "(hbnb)" before each command."""
+	prompt = "(hbnb) "
+	cmds = ["BaseModel"]
 
-    def to_quit(self, arg):
-        return True
+	def do_quit(self, arg):
+		"""This cmd quit the console by returning True"""
+		return True
+
                         
-    def do_EOF(self, arg):                               
-        """Exit the command interpreter (Ctrl+D)"""
-        print("exiting...")  # Add a newline before exiting
-        sys.exit(0)
+	def do_EOF(self, arg):                               
+		"""Exit the command interpreter (Ctrl+D)"""
+		print("exiting...")  # Add a newline before exiting
+		sys.exit(0)
 
-    def emptyline(self):
-        """Do nothing on empty line"""
-        pass
+	def emptyline(self):
+		"""Do nothing on empty line"""
+		pass
     
+	def do_create(self, arg):
+			"""Creates a new instance of BaseModel and save to the Json file"""
+			args = arg.split()
+			if len(args) == 0:
+				print("** class name missing **")
+			else:
+				if args[0] not in HBNBCommand.cmds:
+					print("** class doesn't exist **")
+				else:
+					obj = eval(args[0])()
+					print(obj.id)
+					storage.save()
+					
+	
+
+
+
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
 
