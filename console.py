1be94f4b69ae5993e49eb615b7e3dfb01f6274b9
LeinadMori <iromdaniel@gmail.com>
2023-12-07 16:44:30 +0100

    console.py

--- console.py
+++ console.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 import cmd
+from models.base_model import BaseModel
+from models.user import User
+from storage import FileStorage
+
 
 class HBNBCommand(cmd.Cmd):
     """Prompt the user with "(hbnb)" before each command."""
@@ -20,5 +24,19 @@ class HBNBCommand(cmd.Cmd):
     
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
+
+def do_show_user(self,arg):
+    """Implement the Showing of all users who have access to the system """
+
+def do_creat_user(self,arg):
+    """Implement the Adding new of user to the system"""
+
+def do_destroy_user(self,arg):
+    """Implement the Deleting of some previous users from the system"""
+
+def do_update_user(self,arg):
+    """Implement update of current user"""
+
 
 
