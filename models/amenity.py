8cf7781f96123308828bcdaf4024b350a90a7dd3
imole <dremkay71@gmail.com>
2023-12-08 10:29:15 +0100

    task9: all other Classes added

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+"""this contains the Amenity Class"""
+
+class Amenity(BaseModel):
+	"""Amenity class definition"""
+	name = ""
---
+++ models/place.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+"""this contains the Place Class"""
+
+class Place(BaseModel):
+	"""Place class definition"""
+	city_id = "" # it will be the City.id
+	user_id = "" # it will be the User.id
+	name = ""
+	description = ""
+	number_rooms = 0
+	number_bathrooms = 0
+	max_guest = 0
+	price_by_night = 0
+	latitude = 0.0
+	longitude = 0.0
+	amenity_ids = [] # it will be the list of Amenity.id later
